d0f0b87d75ba724590b9097f54a32bb4a835c92d
LeandraOliv <leandraarruda.oliveira@gmail.com>
2025-02-27 20:42:00 -0300

    Add files via upload

---
+++ Oi, fulano001.py
@@ -0,0 +1,24 @@
+"""
+Programa: Oi, fulano
+Descrição: Pergunte o nome do usuário e apresente a mensagem ”Oi,...”, seguida pelo nome do
+usuário.
+Autor: Leandra Arruda de Oliveira
+Data: 27/02/2025
+Versão: 0.0.1
+"""
+
+# Alocação de memória
+nome_usuario = ""
+frase = ""
+
+
+# Entrada de dados
+nome_usuario = input("\nQual o seu nome?: ")
+
+
+# Processamento de dados
+frase = f"Oi, {nome_usuario}"
+
+
+# Saída de dados
+print(frase)
